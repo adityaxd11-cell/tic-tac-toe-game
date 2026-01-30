da522904308952def2f72b16e98fb8648bf25c15
adityaxd11-cell <adityaxd.11@gmail.com>
2026-01-30 13:45:18 +0530

    Added Tic Tac Toe Js code

---
+++ app.js
@@ -0,0 +1,15 @@
+let boxes = document.querySelectorAll(".box");
+let resetbutton = document.querySelector("#resetbutton");
+
+let turn0 = true;
+const winpatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+
+];
